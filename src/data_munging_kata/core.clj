a50053d3b484df1b29dcfaa379edf5db1edf8e1b
fp <ferak.peter@gmail.com>
2016-03-06 23:22:19 +0100

    added baseball version of the program

--- src/data_munging_kata/core.clj
+++ src/data_munging_kata/core.clj
@@ -3,70 +3,129 @@
 		[clojure.java.io :as io])
   (:gen-class))
 
-; general functions
-
+; shared logic
 (defn read-file [filepath]
 	(str/split (slurp filepath) #"\n"))
 
+(defn find-smallest-difference [file extract parse-line calculate-difference result-item]
+     ((first
+     	(sort-by :diff
+     		(map calculate-difference
+     			(map parse-line
+     				(extract file)))))
+     result-item))
 
-; weather functions
+(defn calculate-difference [data value1 value2]
+	(into data
+		{:diff
+			(java.lang.Math/abs
+				(-
+					(read-string (data value2))
+					(read-string (data value1))))}))
 
-(defn parse-weather-line [line]
+; weather
+(defn parse-weather-line [rawcollumns]
 	(zipmap [:day :min :max]
-		(take 3 (next (str/split line #"(\*\s+)|(\s+)")))))
+		(take 3 rawcollumns)))
 
-(defn filtermap-weather-data [rawdata]
-	(filter #(and (not= 0 (count %)) (number? (read-string (% :day))))
-		(map parse-weather-line rawdata)))
+(defn extract-weather-data [rawdata]
+	(filter #(and (not= 0 (count %)) (number? (read-string (first %))))
+		(map #(next (str/split % #"(\*\s+)|(\s+)"))
+		rawdata)))
 
-(defn filter-for-closest-temperature [data]
-	((first
-		(sort-by :dist
-		(map #(into % {:dist (- (read-string (% :min)) (read-string (% :max)))})
-			data)))
-	:day))
+(defn add-temperature-difference [dayminmax]
+	(into dayminmax {:diff (- (read-string (dayminmax :min)) (read-string (dayminmax :max)))}))
 
 ; main weather function
-; (defn -main [& args] (println (filter-for-closest-temperature (filtermap-weather-data (read-file "/Users/fp/code/data-munging-kata/weather.dat")))))
+; (defn -main [& args]
+; 	(println
+; 		(find-smallest-difference
+; 			(read-file "/Users/fp/code/data-munging-kata/weather.dat")
+; 			extract-weather-data
+; 			parse-weather-line
+; 			add-temperature-difference
+; 			:day
+; 				)))
 
-; football functions
+; football
+(defn parse-football-line [rawcollumns]
+	(into
+		(zipmap
+			[:rank :team]
+			(take 2 rawcollumns))
+		{:goalsfor
+			(first (drop 6 rawcollumns))
+		:goalsagainst
+		(first (drop 7 rawcollumns))}))
 
-(defn extract-values [file]
-		(let [data (filter #(= 9 (count %)) (map (fn [line] (next (str/split line #"(\-+)|(\s+\-\s+)|(\.\s+)|(\s+)"))) file))]
-	    	(println
-			     ((first (sort-by :dist
-			     		(map #(into % {:dist (java.lang.Math/abs (- (read-string (% :goalsagainst)) (read-string (% :goalsfor))))})
-			     			 (map (fn [line] 
-			     			 	(into (zipmap [:rank :team] (take 2 line)) 
-			     			 		{:goalsfor (first (drop 6 line))
-			     			 		:goalsagainst (first (drop 7 line))})
-			     			 	) data))) ) :team))))
+(defn extract-football-data [rawdata]
+	(filter
+		#(= 9 (count %))
+		(map
+			#(next (str/split % #"(\-+)|(\s+\-\s+)|(\.\s+)|(\s+)"))
+		 	rawdata)))
+
+(defn add-goals-difference [data]
+	(calculate-difference data :goalsagainst :goalsfor))
 
 ; main football function
-(defn -main [& args] (extract-values (read-file "/Users/fp/code/data-munging-kata/football.dat")))
+; (defn -main [& args]
+; 	(println
+; 		(find-smallest-difference
+; 			(read-file "/Users/fp/code/data-munging-kata/football.dat")
+; 			extract-football-data
+; 			parse-football-line
+; 			add-goals-difference
+; 			:team)))
 
+; baseball
+(defn extract-baseball-data [rawdata]
+	(filter
+		#(< 11 (count %))
+		(map #(str/split % #"(\s+)")
+		 	rawdata)))
+
+(defn parse-baseball-line [rawcollumns]
+	(into
+		{:team
+			(str/join " " (take (- (count rawcollumns) 11) rawcollumns))}
+		(zipmap [:wins :losses]
+			(take 2 (drop (- (count rawcollumns) 11) rawcollumns)))))
+
+(defn add-win-difference [teamwinslosses]
+	(calculate-difference teamwinslosses :wins :losses))
+
+; main baseball function
+(defn -main [& args]
+	(println
+		(find-smallest-difference
+			(read-file "/Users/fp/code/data-munging-kata/baseball.dat")
+			extract-baseball-data
+			parse-baseball-line
+			add-win-difference
+			:team)))
 
 ; football minimal solution
-; (defn extract-values []
+; (defn find-smallest-difference []
 ; 	(with-open [rdr (io/reader "/Users/fp/code/data-munging-kata/football.dat")]
 ; 		(let [data
 ; 			(filter #(= 9 (count %)) (map (fn [line] (next (str/split line #"(\-+)|(\s+\-\s+)|(\.\s+)|(\s+)"))) (line-seq rdr)))]
 ;     	(println
-; 		     ((first (sort-by :dist
-; 		     		(map #(into % {:dist (java.lang.Math/abs (- (read-string (% :goalsagainst)) (read-string (% :goalsfor))))})
-; 		     			 (map (fn [line] 
-; 		     			 	(into (zipmap [:rank :team] (take 2 line)) 
+; 		     ((first (sort-by :diff
+; 		     		(map #(into % {:diff (java.lang.Math/abs (- (read-string (% :goalsagainst)) (read-string (% :goalsfor))))})
+; 		     			 (map (fn [line]
+; 		     			 	(into (zipmap [:rank :team] (take 2 line))
 ; 		     			 		{:goalsfor (first (drop 6 line))
 ; 		     			 		:goalsagainst (first (drop 7 line))})
 ; 		     			 	) data))) ) :team)))))
 
 
 ; weather minimal solution
-; (defn extract-values []
+; (defn find-smallest-difference []
 ; 	(with-open [rdr (io/reader "/Users/fp/code/data-munging-kata/weather.dat")]
 ;     	(println
-; 		     ((first (sort-by :dist
-; 		     		(map #(into % {:dist (- (read-string (% :min)) (read-string (% :max)))})
+; 		     ((first (sort-by :diff
+; 		     		(map #(into % {:diff (- (read-string (% :min)) (read-string (% :max)))})
 ; 		     			 (filter #(and (not= 0 (count %)) (number? (read-string (% :day))))
 ; 		     			 (map (fn [line] (zipmap [:day :min :max] (take 3 (next (str/split line #"(\*\s+)|(\s+)")))))
 ; 		     				(line-seq rdr)))))) :day))))
