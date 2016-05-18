4b67eb92a866b25bfc1be1b6fe8b19e6a95ce7c7
fp <ferak.peter@gmail.com>
2016-05-18 18:42:44 +0200

    added xml libraries to project.clj

--- project.clj
+++ project.clj
@@ -3,8 +3,11 @@
   :url "http://codekata.com/kata/kata04-data-munging/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :main data-munging-kata.core 
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                [org.clojure/data.xml "0.0.7"]
+                [org.clojure/data.zip "0.1.1"]
+                [clj-tagsoup/clj-tagsoup "0.3.0"]]
+  :main data-munging-kata.core
   :aot [data-munging-kata.core]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
